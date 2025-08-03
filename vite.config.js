deef4a7db0eb65444610b274b819f5e66450dba1
vertiKarl <vertikarl@proton.me>
2025-08-03 15:10:27 +0200

    added share route to production

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import { resolve } from "node:path";
 
 export default {
   base: "/anilist-smashorpass",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        share: resolve(__dirname, "share/index.html"),
+      },
+    },
+  },
 };
